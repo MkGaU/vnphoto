e215642c0f479ad3faa4fb7a753e5c3e8b1d4993
vietnamphoto <erikp.vn@gmail.com>
2013-10-08 16:08:46 +0700

    Revert "this is new code for controller"
    This reverts commit f070bf7df93bb4c95d684dac22a3edeeeef65151.

---
+++ themes/newPHPClass.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of newPHPClass
+ *
+ * @author Viet
+ */
+class newPHPClass {
+    //put your code here
+}
+
+?>
