84d4e1e1ced328aab5d7eb768a23ef2a43816611
vietnamphoto <erikp.vn@gmail.com>
2013-10-08 15:17:59 +0700

    this is new code for controller

---
+++ protected/controllers/sad.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of sad
+ *
+ * @author Viet
+ */
+class sad {
+    //put your code here
+}
+
+?>
